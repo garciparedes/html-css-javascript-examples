29a05bcb857d588586d603356ed59ad9033e9afc
Sergio García <garciparedes@gmail.com>
2015-11-26 15:56:31 +0100

    Fixed bugs

--- d3js-examples/block3/example4/js/script.js
+++ d3js-examples/block3/example4/js/script.js
@@ -4,23 +4,30 @@ var CSVFileName = "data.csv"
 
 function d3BarChartExample(){
 
+    //
     // Definimos el valor que le queremos dar
     // a los margenes.
+    //
+    // Definimos el valor que tendran
+    // el ancho y alto del grafico.
+    //
+    // Tambien el color de las barras del grafico
+    //
     var margin = {
         top: 50,
         right: 20,
         bottom: 30,
         left:40
     }
-    // Definimos el valor que tendran
-    // el ancho y alto del grafico.
-    var w = 500 - margin.left - margin.right;
-    var h = 350 - margin.top - margin.bottom;
 
-    // Tambien el color de las barras del grafico
+    var w = 640 - margin.left - margin.right;
+    var h = 480 - margin.top - margin.bottom;
+
     var color = d3.scale.category10();
 
 
+
+    //
     // Definimos x e y, que seran las funciones
     // encargadas de reescalar los valores que introduzcamos
     // en el grafico para que "encajen" dentro de las
@@ -30,6 +37,7 @@ function d3BarChartExample(){
     // va a ser categorica ( el nombre de la fruta)
     // En el caso de la y sin embargo utilizaremos linear porque
     // utilizaremos valores numericos, es decir, el precio.
+    //
     var x = d3.scale.ordinal()
         .rangeRoundBands([0,w], .1);
 
@@ -37,22 +45,22 @@ function d3BarChartExample(){
         .range([h, 0]);
 
 
+
+    //
     // Definimos los ejes x e y
     // Cada uno se reescala a partir de la funcion correspondiente
     // definida en las lineas superiores y a la vez se orientan según
     // corresponde.
     //
     // Tambien vamos a incluid un grid en el eje de las y.
+    //
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom");
 
-    var formatPercent = d3.format(".0%");
-
     var yAxis = d3.svg.axis()
         .scale(y)
-        .orient("left")
-        .tickFormat(formatPercent);
+        .orient("left");
 
     var yGrid = d3.svg.axis()
         .scale(y)
@@ -63,13 +71,14 @@ function d3BarChartExample(){
 
 
 
-
+    //
     // Definimos el objecto svg en la el cuerpo de la web.
     // Ademas le asignamos el ancho y el alto.
     // Tambien le agregamos un grupo y "transfomamos" el eje
     // de origen (en svg arriba a la izquierda por defecto) en
     // abajo a la izquierda para simplificar la colocacion de los
     // valores del grafico.
+    //
     var svg = d3.select("body").append("svg")
         .attr("width", w + margin.left + margin.right)
         .attr("height", h + margin.top + margin.bottom)
@@ -78,6 +87,9 @@ function d3BarChartExample(){
             , "translate("+ margin.left +", " + margin.top + ")"
         );
 
+
+
+    //
     // Leemos los datos del fichero json donde estan alojados
     // para despues poder incorporarlos a nuestro grafico.
     // Lo que hacemos con el codigo "+d.Precio" es convertir
@@ -90,23 +102,26 @@ function d3BarChartExample(){
     // y el numero de elementos que hay en el caso de la x. Tambien
     // incluimos un grid en el eje y.
     //
+    // Tambien incluimos unos labels de titulo y escala en el eje y.
     //
+    // Por ultimo generamos las barras del diagrama de barras
     //
     d3.csv(dataRoute + CSVFileName, function(error, data) {
 
-        var sum = 0;
+
         data.forEach(function(d) {
             d.Precio = +d.Precio;
-            sum += d.Precio;
         });
+
 
         x.domain(data.map(function(d){
             return d.Fruta
         }));
 
         y.domain([0, d3.max(data, function(d){
-            return d.Precio/sum;
+            return d.Precio;
         })]);
+
 
 
         svg.append("g")
@@ -123,6 +138,7 @@ function d3BarChartExample(){
             .call(yGrid);
 
 
+
         var labels = svg.append("g")
             .attr("class", "labels");
 
@@ -131,7 +147,8 @@ function d3BarChartExample(){
             .attr("y", 6)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
-            .text("Precio ");
+            .text("Precio (€)");
+
 
         var title = svg.append("g")
             .attr("class", "title");
@@ -144,24 +161,15 @@ function d3BarChartExample(){
             .text("My first bar chart");
 
 
+
         svg.selectAll(".bar")
             .data(data)
             .enter().append("rect")
             .attr("class", "bar")
             .attr("x", function(d) { return x(d.Fruta); })
             .attr("width", x.rangeBand())
-            .attr("y", function(d) {
-                //TODO Solve it
-                return y(d.Precio/sum);
-            })
-            .attr("height", function(d) {
-                //TODO Solve it
-
-                return h - y(d.Precio/sum);
-            })
+            .attr("y", function(d) { return y(d.Precio);})
+            .attr("height", function(d) { return h - y(d.Precio);})
             .attr("fill", function(d ){ return color(d.Fruta);});
      });
-
-
-
 }
